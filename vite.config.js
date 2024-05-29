fbb7d0bd36cb2dd02ebda5aa040f86a25f6edf2f
thorcazo <thorcazodesign@gmail.com>
2024-05-29 09:16:34 +0200

    refactor: Update dev script to remove the --open flag

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/', // Cambia esto a tu base URL si es diferente
   server: {
     port: 4321
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['phaser']
+        }
+      }
+    }
   }
 });
